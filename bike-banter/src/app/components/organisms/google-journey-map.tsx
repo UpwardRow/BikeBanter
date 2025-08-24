538bb67b4734cf6ecb32d9a989c1c2a2a671d089
Adam <downeyadam0312@gmail.com>
2025-08-24 20:28:51 +0100

    Homepage Grid Features and Cleanup
    I cleaned up some code and changed the position of the grids for the
    homepage. They now also have headings, I may add more grids as I push up
    more to this branch

--- bike-banter/src/app/components/organisms/google-journey-map.tsx
+++ bike-banter/src/app/components/organisms/google-journey-map.tsx
@@ -2,7 +2,7 @@ import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 export default function UserGoogleMap() {
     return(
-        <div className="user-google-map" style={{ width: '97vw', height: '55vh'}}>
+        <div className="user-google-map" style={{ width: '97vw', height: '58vh'}}>
             <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
                 <Map
                 style={{ width: '100%', height: '100%' }}
