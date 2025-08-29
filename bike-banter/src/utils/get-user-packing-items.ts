5d55d97e58a03ccacbad9235c88580604c48271d
Adam <downeyadam0312@gmail.com>
2025-08-29 23:05:18 +0100

    Added packing items fetch file

---
+++ bike-banter/src/utils/get-user-packing-items.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function getUserPackingItems(): Promise<string[]> {
+    const { data, error } = await supabase
+    .from('PackingItems')
+    .select('*')
+
+    if (error) {
+        console.error('Error fetching packing items:', error.message)
+        return []
+    }
+
+    return data
+}
